977f0597ba93f7cfb68dc82e8f73db2865ba94f7
Martin <nnorcco@gmail.com>
2023-07-17 19:49:24 +0300

    app.vue update

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@
 <script lang="ts" setup>
 useHead({
   titleTemplate: (title) => `${title}`,
-  meta: [{ name: 'description', content: "TomovBlogs - Tech Research Blog - Martin Tomov" }],
+  meta: [{ name: 'description', content: "Blog | Martin Tomov - Tech Research - Martin Tomov" }],
   link: [{ rel: 'icon', type: 'image/png', href: `${useRuntimeConfig().app.baseURL}favicon.ico` }],
   htmlAttrs: { lang: "en" },
   bodyAttrs: { class: "bg-white dark:bg-black text-[#555555] dark:text-[#bbbbbb]" },
